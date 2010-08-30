0251cedb1916fd2141548c34838422d5bc8b26cc
phudgins <pete.hudgins@gmail.com>
2010-08-30 16:40:59 -0400

    Done with sign in

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -35,4 +35,20 @@ describe "Users" do
       end
     end
   end
+
+  describe "sign in/out" do
+
+    describe "success" do
+      it "should sign a user in and out" do
+        user = Factory(:user)
+        visit signin_path
+        fill_in :email,     :with => user.email
+        fill_in :password,  :with => user.password
+        click_button
+        controller.should be_signed_in
+        click_link "Sign out"
+        controller.should_not be_signed_in
+      end
+    end
+  end
 end
